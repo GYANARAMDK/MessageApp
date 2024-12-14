237d7ab31e65cd11da44fd36eb3c94a86cd59ccf
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-12-15 03:29:35 +0530

    modified posts.jsx and main.jsx

--- src/assets/Components/Posts.jsx
+++ src/assets/Components/Posts.jsx
@@ -18,15 +18,12 @@ export default function Posts() {
   const [open, setOpen] = useState(false);
 
   const Dispatch = useDispatch();
-  const token = useSelector(state=>state.Outh.token)
+  const token = useSelector((state) => state.Outh.token);
   const posts = useSelector((state) => state.Post.post);
   const user = useSelector((state) => state.Outh.user);
 
-  const [like, setlike] = useState();
-  
   useEffect(() => {
     const fetchpost = async () => {
-      console.log(token)
       try {
         const response = await axios.get(
           "https://instaclone-1-187b.onrender.com/api/v1/post/allpost",
@@ -38,23 +35,22 @@ export default function Posts() {
           }
         );
         if (response.status === 200) {
-          console.log("helooooooo");
+          console.log(response.data.posts);
+          Dispatch(setPost(response.data.posts));
         }
-        Dispatch(setPost(response.data.posts));
-        console.log(response.data.posts);
-        console.log(posts);
       } catch (error) {
         console.log(error);
+        if (error.response) alert(error.response.data.message);
       }
     };
     fetchpost();
   }, []);
-  
+
   const ChangeEventHandle = (e) => {
     e.preventDefault();
     settext(e.target.value);
   };
- 
+
   const likeDislikeHandle = async (id) => {
     const targetpost = posts.find((post) => post._id === id);
     let liked = targetpost ? targetpost.likes.includes(user._id) : false;
@@ -78,7 +74,7 @@ export default function Posts() {
               ...post,
               likes:
                 action === "like"
-                  ? [...new Set([...post.likes, user._id])] 
+                  ? [...new Set([...post.likes, user._id])]
                   : post.likes.filter((uid) => uid !== user._id),
             };
           }
@@ -89,143 +85,147 @@ export default function Posts() {
       // alert(response.data.message);
     } catch (error) {
       console.log(error);
-      if(error.response) alert(error.response.data.message)
+      if (error.response) alert(error.response.data.message);
     }
   };
-   const CommentHandle=async(id)=>{
-        try {
-          const response= await axios.post(`https://instaclone-1-187b.onrender.com/api/v1/post/${id}/comment/add`,{text},
-            {
-
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-              withCredentials: true,
-            }
-          );
-          alert(response.data.message)
-          if (response.status === 201) {
-            const updatedPosts = posts.map((post) => {
-              if (post._id === id) {
-                return {
-                  ...post,
-                  comments:
-                       [response.data.comment, ...post.comments]
-                      
-                };
-              }
-              return post;
-            });
-            Dispatch(setPost(updatedPosts));
-            settext('')
-          }
-        } catch (error) {
-          console.log(error)
-          if(error.response)
-            alert(error.response.data.message)
+  const CommentHandle = async (id) => {
+    try {
+      const response = await axios.post(
+        `https://instaclone-1-187b.onrender.com/api/v1/post/${id}/comment/add`,
+        { text },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          withCredentials: true,
         }
-   }
+      );
+      alert(response.data.message);
+      if (response.status === 201) {
+        const updatedPosts = posts.map((post) => {
+          if (post._id === id) {
+            return {
+              ...post,
+              comments: [response.data.comment, ...post.comments],
+            };
+          }
+          return post;
+        });
+        Dispatch(setPost(updatedPosts));
+        settext("");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response) alert(error.response.data.message);
+    }
+  };
   return (
     <>
-      <div>
-        {posts &&
-          posts.map((item) => {
-            return (
-              <div
-                className="max-w-xl mx-auto bg-white border rounded-lg shadow-lg my-4 ml-4  "
-                key={item._id}
-              >
-                {/* Profile Section */}
-                <div className="flex items-center p-4">
-                  {/* Avatar */}
-                  <NavLink to={`/profile/${user.id}`}>
+      {posts &&
+        posts.map((item) => {
+          return (
+            <div
+              className="max-w-xl mx-auto bg-white border rounded-lg shadow-lg my-4 ml-4  "
+              key={item._id}
+            >
+              {/* Profile Section */}
+              <div className="flex items-center p-4">
+                {/* Avatar */}
+                <NavLink to={`/profile/${item.author._id}`}>
                   <img
                     src={item.author.profilepicture}
                     alt="Profile"
                     className=" rounded-full w-auto  aspect-square  object-cover max-h-12 mr-4"
                   />
-                 </NavLink>
-                  <div>
-                    <p className="font-semibold">{item.author.name}</p>
-                    <p className="text-sm text-gray-500">
-                      {item.createdAt?.split("T")[0] || "first day"}
-                    </p>
-                  </div>
-                </div>
-
-                {/* Image */}
-                <div className="relative">
-                  <img
-                    src={item.image}
-                    alt="Post"
-                    className="w-full rounded object-cover"
-                  />
-                </div>
-
-                {/* Post Info */}
-                <div className="p-4">
-                  {/* Like, Comment, Share, and Save Icons */}
-                  <div className="flex justify-between items-center">
-                    <div className="flex space-x-4">
-                      <FaHeart
-                        className={`text-2xl cursor-pointer ${
-                          item.likes.includes(user._id) ? "text-red-500" : "text-gray-800"
-                        } `}
-                        onClick={() => {
-                          likeDislikeHandle(item._id);
-                        }}
-                      />
-                      <FaComment 
-                        onClick={() => { Dispatch(setselectedpost(item))
-                          setOpen(true);
-                        }}
-                        className="text-gray-800 text-2xl cursor-pointer"
-                      />
-                      <FaShare className="text-gray-800 text-2xl cursor-pointer" />
-                    </div>
-                    <FaBookmark className="text-gray-800 text-2xl cursor-pointer" />
-                  </div>
-
-                  {/* Likes and Comments */}
-                  <p className="mt-2 text-gray-700 font-semibold">
-                    {item.likes.length}
+                </NavLink>
+                <div>
+                  <p className="font-semibold">{item.author.name}</p>
+                  <p className="text-sm text-gray-500">
+                    {item.createdAt?.split("T")[0] || "first day"}
                   </p>
-                  <p className="mt-1 text-gray-600">
-                    <span className="font-semibold">{item.author.name}</span>{" "}
-                    {item.caption}
-                  </p>
+                </div>
+              </div>
 
-                  {/* Comment Section */}
-                  <div className="mt-2 text-gray-600">
-                    <p
-                      className="text-sm cursor-pointer"
-                      onClick={() => { Dispatch(setselectedpost(item))
+              {/* Image */}
+              <div className="relative">
+                <img
+                  src={item.image}
+                  alt="Post"
+                  className="w-full rounded object-cover"
+                />
+              </div>
+
+              {/* Post Info */}
+              <div className="p-4">
+                {/* Like, Comment, Share, and Save Icons */}
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-4">
+                    <FaHeart
+                      className={`text-2xl cursor-pointer ${
+                        item.likes.includes(user._id)
+                          ? "text-red-500"
+                          : "text-gray-800"
+                      } `}
+                      onClick={() => {
+                        likeDislikeHandle(item._id);
+                      }}
+                    />
+                    <FaComment
+                      onClick={() => {
+                        Dispatch(setselectedpost(item));
                         setOpen(true);
                       }}
-                    >{`View all ${item.comments.length} comments`}</p>
-                    <Comments open={open} setOpen={setOpen} />
-                    <div className="flex items-center justify-between">
-                      <input
-                        type="text"
-                        placeholder="add a comment"
-                        className="flex-grow border rounded-full px-4 py-2 focus:outline-none text-sm"
-                        onChange={(e) => {
-                          ChangeEventHandle(e);
-                        }}
-                        value={text}
-                      />
-                      {text && (
-                        <button className="ml-2 bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600">
-                          <FaPaperPlane onClick={()=>{CommentHandle(item._id)}} />
-                        </button>
-                      )}
-                    </div>
+                      className="text-gray-800 text-2xl cursor-pointer"
+                    />
+                    <FaShare className="text-gray-800 text-2xl cursor-pointer" />
+                  </div>
+                  <FaBookmark className="text-gray-800 text-2xl cursor-pointer" />
+                </div>
+
+                {/* Likes and Comments */}
+                <p className="mt-2 text-gray-700 font-semibold">
+                  {item.likes.length}
+                </p>
+                <p className="mt-1 text-gray-600">
+                  <span className="font-semibold">{item.author.name}</span>{" "}
+                  {item.caption || 'Message Me for web development'}
+                </p>
+
+                {/* Comment Section */}
+                <div className="mt-2 text-gray-600">
+                  <p
+                    className="text-sm cursor-pointer"
+                    onClick={() => {
+                      Dispatch(setselectedpost(item));
+                      setOpen(true);
+                    }}
+                  >{`View all ${item.comments.length} comments`}</p>
+                  <Comments open={open} setOpen={setOpen} />
+                  <div className="flex items-center justify-between">
+                    <input
+                      type="text"
+                      placeholder="add a comment"
+                      className="flex-grow border rounded-full px-4 py-2 focus:outline-none text-sm"
+                      onChange={(e) => {
+                        ChangeEventHandle(e);
+                      }}
+                      value={text}
+                    />
+                    {text && (
+                      <button className="ml-2 bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600">
+                        <FaPaperPlane
+                          onClick={() => {
+                            CommentHandle(item._id);
+                          }}
+                        />
+                      </button>
+                    )}
                   </div>
                 </div>
               </div>
-            );
-          })}
-      </div>
+            </div>
+          );
+        })}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,11 +5,11 @@ import { Provider } from "react-redux";
 import {PersistGate} from 'redux-persist/integration/react'
 import { Store, persistor } from "./Redux/Store.js";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <Provider store={Store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </StrictMode>
+  </>
 );
