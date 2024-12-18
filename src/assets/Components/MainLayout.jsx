530a9cb4f1a0b0cbaaec466016c4a56fa667363b
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-12-19 04:20:28 +0530

    problem soled

--- src/assets/Components/MainLayout.jsx
+++ src/assets/Components/MainLayout.jsx
@@ -6,34 +6,34 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 
 export default function MainLayout() {
-  // const Navigate = useNavigate();
-  // const token = useSelector((state) => state.Outh.token);
-  // useEffect(() => {
-  //   if (!token) Navigate("/Login");
-  // }, [token,Navigate]);
-  // return (
-  //   <>
-      
-  //     {(token)? (
-  //       <div className="flex h-screen overflow-hidden">
-  //         <div className="w-1/4 bg-gray-200 h-full">
-  //           <LeftSideBar />
-  //         </div>
-  //         <div className="flex-grow overflow-y-auto">
-  //           <Outlet />
-  //         </div>
-  //       </div>
-  //     ):
-  //     <div className="flex items-center justify-center bg-red-500">
-  //                <a href="facebook.com"></a>
-  //     </div>
-  //   }
-
-  //   </>
-  // );
+  const Navigate = useNavigate();
+  const token = useSelector((state) => state.Outh.token);
+  useEffect(() => {
+    if (!token) Navigate("/Login");
+  }, [token,Navigate]);
   return (
     <>
-    <h1>how are you </h1>
+      
+      {(token) ? (
+        <div className="flex h-screen overflow-hidden">
+          <div className="w-1/4 bg-gray-200 h-full">
+            <LeftSideBar />
+          </div>
+          <div className="flex-grow overflow-y-auto">
+            <Outlet />
+          </div>
+        </div>
+      ):
+      <div className="flex items-center justify-center bg-red-500">
+                 <a href="facebook.com"></a>
+      </div>
+    }
+
     </>
-  )
+  );
+  // return (
+  //   <>
+  //   <h1>how are you </h1>
+  //   </>
+  // )
 }
